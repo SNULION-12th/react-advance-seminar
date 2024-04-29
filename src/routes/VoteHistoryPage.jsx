f6b428fa4c253528766159f3f3e8dd19a73dc5a3
Taehyeungkim <kth1837@snu.ac.kr>
2024-04-29 20:27:16 +0900

    :sparkles: finished getting vote history

--- src/routes/VoteHistoryPage.jsx
+++ src/routes/VoteHistoryPage.jsx
@@ -24,12 +24,14 @@ function VoteHistoryPage() {
         },
       });
       const data = response.data;
+
       const imageSet = [];
 
       data.map((e) => {
         imageSet.push({
           url: e.image.url,
           value: e.value,
+          id: e.image_id,
         });
       });
 
@@ -65,12 +67,12 @@ function VoteHistoryPage() {
       <div className="w-2/3 h-2/3 p-5 border-4 rounded-2xl border-[#FF6841] flex justify-center items-center">
         <div className="w-full h-3/4 grid grid-cols-4 auto-rows-[46%] gap-4 overflow-y-scroll hide-scroll scrollable-content">
           {images.map((img) => (
-            <div className="w-full h-full relative">
+            <div key={img.id} className="w-full h-full relative">
               <img
                 key={img.url}
                 src={img.url}
                 className={`object-cover w-full h-full border-[3px] border-[#FF6841] rounded-xl 
-                  ### FILL ME ### 
+                  ${img.value === 1 ? "border-red-600" : "border-blue-600"}
                 `}
               />
             </div>
