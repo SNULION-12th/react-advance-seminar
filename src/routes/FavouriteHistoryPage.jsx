1fdac7646aa6879b4eb52dc99b8f3dded6cf9577
JINSEO <jinseo040201@snu.ac.kr>
2024-04-30 19:08:03 +0900

    .

--- src/routes/FavouriteHistoryPage.jsx
+++ src/routes/FavouriteHistoryPage.jsx
@@ -16,6 +16,30 @@ function FavouriteHistoryPage() {
   const getImages = async () => {
     try {
       // ### TO DO ###
+      const response = await axios.get(
+        `https://api.thecatapi.com/v1/favourites?sub_id=${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key":
+              "live_uJIAOk7Z9jClOkYFxqI4332RzBXawDl1l8L0n9wZehDHhuIW2a7rL92pd2AzyXCX",
+          },
+        }
+      );
+
+      const data = response.data;
+      const imageSet = [];
+
+      data.map((e) => {
+        imageSet.push({
+          id: e.image.id,
+          url: e.image.url,
+          isFavourite: true,
+          favouriteId: e.id,
+        });
+      });
+
+      setImages(imageSet);
       // #############
     } catch (err) {
       console.log(err);
