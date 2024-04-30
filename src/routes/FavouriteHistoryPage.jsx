0ed38b0ff2b4b510a91efb158ad198b675db7c12
minseo25 <ms059333@gmail.com>
2024-04-30 20:05:58 +0900

    feat:seminar

--- src/routes/FavouriteHistoryPage.jsx
+++ src/routes/FavouriteHistoryPage.jsx
@@ -15,8 +15,30 @@ function FavouriteHistoryPage() {
 
   const getImages = async () => {
     try {
-      // ### TO DO ###
-      // #############
+      const response = await axios.get(
+        `https://api.thecatapi.com/v1/favourites?sub_id=${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key":
+              "live_cD81TkTWWcQs2vA5Xm2tqn3UZrbQkaxX4UzSK3WSFLUW1khBlU2WmkNA7k9JJpqj",
+          },
+        }
+      );
+
+      const data = response.data;
+      const imageSet = [];
+
+      data.map((e) => {
+        imageSet.push({
+          id: e.image.id,
+          url: e.image.url,
+          isFavourite: true,
+          favouriteId: e.id,
+        });
+      });
+
+      setImages(imageSet);
     } catch (err) {
       console.log(err);
     }
@@ -34,7 +56,7 @@ function FavouriteHistoryPage() {
           headers: {
             "Content-Type": "application/json",
             "x-api-key":
-              "live_GKblu8slVg2fFDula9hfgUUWLXlaX6aCWLZpv8pAyFb6Cyhxzq9CkhlwW88Erb0z",
+              "live_cD81TkTWWcQs2vA5Xm2tqn3UZrbQkaxX4UzSK3WSFLUW1khBlU2WmkNA7k9JJpqj",
           },
         }
       );
@@ -65,7 +87,7 @@ function FavouriteHistoryPage() {
           headers: {
             "Content-Type": "application/json",
             "x-api-key":
-              "live_GKblu8slVg2fFDula9hfgUUWLXlaX6aCWLZpv8pAyFb6Cyhxzq9CkhlwW88Erb0z",
+              "live_cD81TkTWWcQs2vA5Xm2tqn3UZrbQkaxX4UzSK3WSFLUW1khBlU2WmkNA7k9JJpqj",
           },
         }
       );
