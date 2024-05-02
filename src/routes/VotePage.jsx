1c4ab070bd0fbd0dc55c700467cda3413cfae032
Atipico1 <athjk3@snu.ac.kr>
2024-05-03 00:00:22 +0900

    Feat: HW

--- src/routes/VotePage.jsx
+++ src/routes/VotePage.jsx
@@ -6,7 +6,7 @@ import { getCookie } from "../utils/cookie";
 function VotePage() {
   const navigate = useNavigate();
   const userId = getCookie("userId");
-
+  const [image, setImage] = useState({});
   const [thumbsUpImage, setThumbsUpImage] = useState(
     require("../assets/images/thumbs-up-icon.png")
   );
@@ -20,8 +20,24 @@ function VotePage() {
 
   const getImage = async () => {
     try {
-      // ### TO DO ###
-      // #############
+      const response = await axios.get(
+        "https://api.thecatapi.com/v1/images/search?limit=1&size=full",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key":
+              "live_Iyhi1ILrF0FD1vlsdHpX4CYBM37yG1ZXsUjP58Jx4Wov7qsp27cZmsuBjMt0HYVo",
+          },
+        }
+      );
+
+      const data = response.data;
+      setImage({
+        id: data[0].id,
+        url: data[0].url,
+        isFavorite: false,
+        favoriteId: null,
+      });
     } catch (err) {
       console.log(err);
     }
@@ -29,8 +45,21 @@ function VotePage() {
 
   const vote = async (val) => {
     try {
-      // ### TO DO ###
-      // #############
+      const response = await axios.post(
+        "https://api.thecatapi.com/v1/votes",
+        {
+          image_id: image.id,
+          sub_id: userId,
+          value: val,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key":
+              "live_Iyhi1ILrF0FD1vlsdHpX4CYBM37yG1ZXsUjP58Jx4Wov7qsp27cZmsuBjMt0HYVo",
+          },
+        }
+      );
     } catch (err) {
       console.log(err);
     }
@@ -63,6 +92,7 @@ function VotePage() {
         <div className="w-full h-[90%] flex justify-evenly items-center">
           <img
             // ### ONE CAT IMAGE ###
+            src={image.url}
             className="w-3/5 h-full border-[3px] rounded-xl border-[#FF6841]"
           />
           <div className="w-1/3 flex gap-12 justify-center">
@@ -70,11 +100,31 @@ function VotePage() {
               src={thumbsUpImage}
               className="w-20 h-20 cursor-pointer"
               // ### thumbsUpImage Event ###
+              onMouseEnter={() =>
+                setThumbsUpImage(
+                  require("../assets/images/thumbs-up-click.png")
+                )
+              }
+              onMouseLeave={() =>
+                setThumbsUpImage(require("../assets/images/thumbs-up-icon.png"))
+              }
+              onClick={() => vote(1)}
             />
             <img
               src={thumbsDownImage}
               className="w-20 h-20 cursor-pointer"
               // ### thumbsDownImage Event ###
+              onMouseEnter={() =>
+                setThumbsDownImage(
+                  require("../assets/images/thumbs-down-click.png")
+                )
+              }
+              onMouseLeave={() =>
+                setThumbsDownImage(
+                  require("../assets/images/thumbs-down-icon.png")
+                )
+              }
+              onClick={() => vote(-1)}
             />
           </div>
         </div>
